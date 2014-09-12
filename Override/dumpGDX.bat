9fc1f74d69c0939d1e6531428bf38b82b578092b
unknown <bishopp@ECOM140.ecom.local>
2014-09-12 15:03:26 +1200

    Codes in this commit are consistent with audited v1.4 - Sept 2014

--- Override/dumpGDX.bat
+++
@@ -1,3 +0,0 @@
-rem	Dump all GDX files in the current directory into .gms files
-
-for %%f in (*.gdx) do gdxdump "%%f" > "%%~nf.gms"
--- Override/reloadGDX.bat
+++
@@ -1,4 +0,0 @@
-rem	Convert or reload all .gms files in the current directory to GDX files
-
-for %%f in (*.gms) do gams "%%f" gdx="%%~nf.gdx"
-del *.lst
