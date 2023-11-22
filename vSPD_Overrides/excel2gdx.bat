c2477bc8e3ee90717676d741d3abf2bbe61915c5
Tuong Nguyen <tuong.nguyen@ea.govt.nz>
2023-11-23 10:33:30 +1300

    Update excel2gdx.bat

--- vSPD_Overrides/excel2gdx.bat
+++ vSPD_Overrides/excel2gdx.bat
@@ -1,9 +1,3 @@
 cd /D "%~dp0"
 gdxxrw vSPD_Overrides.xlsx output="../Override/vSPD_Overrides" squeeze=n @howtoread.inc
-
-del "*.lst"
-del "*.lxi"
-del "*.log"
-del "*.put"
-del "*.txt"
-*pause
+pause
